79c4fbf6dd46a355ca6915bc1b40ebe559858fc0
Kamran <87352040+kamran-pog@users.noreply.github.com>
2025-01-23 16:23:21 -0800

    Added get by id

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ app.get('/groceries', async (req, res) => {
   }
 });
 
+app.get('/groceries:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        const query = 'SELECT * FROM groceries WHERE id = $1';
+        const result = await db.query(query, [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).send('Grocery not found');
+        }
+
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+} )
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
