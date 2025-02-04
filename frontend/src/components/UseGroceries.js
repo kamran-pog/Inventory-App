2ab6c21d182107458e71e4b3a3bc142260c7a433
Kamran <87352040+kamran-pog@users.noreply.github.com>
2025-02-04 13:18:42 -0800

    fixed error again

--- frontend/src/components/UseGroceries.js
+++ frontend/src/components/UseGroceries.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { API_URL } from "../API_URL";
+import { API_URL } from "../config/config.js";
 
 function useGroceries() {
   const [groceries, setGroceries] = useState([]);
